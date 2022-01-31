9d85e155cd8f5e6f505e7a39f5771c7b6dc5c53e
linnalessandra <linnalessandra@outlook.com>
2022-01-31 16:20:57 +0100

    allow cross o

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,20 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const secretKey = process.env.STRIPE_SECRET_KEY;
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const port = process.env.PORT || 3000
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
