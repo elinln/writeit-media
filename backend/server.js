bfcaa0a92a9d8af40c2dd348968c8e104700aaff
linnalessandra <linnalessandra@outlook.com>
2022-02-01 20:22:28 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,6 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 const port = process.env.PORT || 3000
 
-/*const whitelist = ["http://localhost:3000"]
-/*const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))*/
 
 app.use(cors());
 
