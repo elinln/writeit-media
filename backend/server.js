5bdd303a3dc4da8ab2c30fc3d223ab898dcb0746
elinln <linneaelinnilsson@gmail.com>
2022-02-02 22:36:48 +0100

    changed stripe things

--- backend/server.js
+++ backend/server.js
@@ -16,20 +16,22 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-
+/* 
 var corsOptions = {
     origin: "http://localhost:3001"
 };
 
-app.use(cors(corsOptions));
-
+app.use(cors(corsOptions)); */
 
 const workoutRouter = require('./routes/app.routes')
 const usersRouter = require('./routes/users.routes')
-const checkoutRouter = require('./routes/checkout.routes')
+/* const checkoutRouter = require('./routes/checkout.routes')
+ */
 app.use('/tutorials', workoutRouter);
 app.use('/users', usersRouter);
-app.use('/checkout', checkoutRouter)
+/* app.use('/checkout', checkoutRouter)
+ */
+const priceDB = { '1': 1000, '2': 3000, '3': 5000, '4': 599 }
 
 
 
@@ -45,3 +47,33 @@ app.listen(PORT, () => {
 });
 
 
+app.post('/create-checkout-session', async (req, res) => {
+    const cartItems = JSON.parse(req.body.cartItems);
+    const session = stripe.checkout.sessions.create({
+        line_items: cartItems.map(item => {
+            const li = {
+                price_data: {
+                    currency: 'sek',
+                    product_data: {
+                        name: item.title,
+                        description: item.description,
+
+                    },
+                    unit_amount: priceDB[item.id] * 100
+                },
+                quantity: item.qty,
+            }
+            console.log('Item:', li)
+            return li
+        }),
+        mode: 'payment',
+        success_url: 'http://localhost:3001/successfullOrder',
+        cancel_url: 'http://localhost:3001/cancelledOrder',
+    }).then(session => {
+        res.redirect(303, session.url);
+
+    })
+        .catch(error => {
+            console.error("wrooooonggg:", error)
+        })
+})
