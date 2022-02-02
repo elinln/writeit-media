c47ba79a3ddaacb72e8d79229f9dfd0dd240797e
linnalessandra <linnalessandra@outlook.com>
2022-02-02 13:48:50 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,12 @@ const cors = require('cors');
 /*const app = express();*/
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
