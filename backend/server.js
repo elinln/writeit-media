20cfbd0cd9a34749043ece987d9162a00473254c
linnalessandra <linnalessandra@outlook.com>
2022-02-03 22:04:45 +0100

    serv

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser")
 const port = process.env.PORT || 3000
 const cors = require("cors")
 app.use(cors());
-app.use(express.static("public")); 
+/*app.use(express.static("public")); */
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
